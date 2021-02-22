da11c0937bee413d38e7b6b0cd138f9c3c8a3246
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-02-21 19:11:28 -0500

    Extract data from US and WW

--- script.py
+++ script.py
@@ -4,6 +4,25 @@ import json
 WW_trends = json.loads(open('datasets/WWTrends.json').read())
 US_trends = json.loads(open('datasets/USTrends.json').read())
 
+# Pretty-printing the results
+print("WW trends:")
+print (json.dumps(WW_trends, indent=1))
+
+print("\n", "US trends:")
+print (json.dumps(US_trends, indent=1))
+
+#Extracting all the WW trend names from WW_trends
+world_trends = set([trend['name']                    
+                    for trend in WW_trends[0]['trends']])
+
+#Extracting all the US trend names from US_trends
+us_trends = set([trend['name'] 
+                     for trend in US_trends[0]['trends']])
+
+#Getting the intersection of the two sets
+common_trends = world_trends = world_trends.intersection(us_trends)
+
 #Inspecting data
-print(WW_trends)
-print(US_trends)
+print(world_trends, "\n")
+print(us_trends, "\n")
+print (len(common_trends), "common trends:", common_trends)
