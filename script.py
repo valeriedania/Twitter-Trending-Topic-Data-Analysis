829d07b1079b7de99e535c0bd19beff3c59d5b09
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-02-21 19:23:17 -0500

    Extract tweet, user, & hashtag

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import json
 
-#Load WW_trends and US_trends
+#Loaading WW_trends and US_trends
 WW_trends = json.loads(open('datasets/WWTrends.json').read())
 US_trends = json.loads(open('datasets/USTrends.json').read())
 
@@ -26,3 +26,28 @@ common_trends = world_trends = world_trends.intersection(us_trends)
 print(world_trends, "\n")
 print(us_trends, "\n")
 print (len(common_trends), "common trends:", common_trends)
+
+#Loading data from #WeLoveTheEarth dataset
+tweets = json.loads(open('datasets/WeLoveTheEarth.json').read())
+                   
+#Inspecting some tweets from dataset
+tweets[0:2]
+
+# Extracting the text of all the tweets from the tweet object
+texts = [tweet['text'] 
+                 for tweet in tweets ]
+
+# Extracting screen names of users tweeting about #WeLoveTheEarth
+names = [user_mention['screen_name'] 
+                 for tweet in tweets
+                     for user_mention in tweet['entities']['user_mentions']]
+
+# Extracting all the hashtags being used when talking about this topic
+hashtags = [hashtag['text'] 
+             for tweet in tweets
+                 for hashtag in tweet['entities']['hashtags']]
+
+# Inspecting the first 10 results
+print (json.dumps(texts[0:10], indent=1),"\n")
+print (json.dumps(names[0:10], indent=1),"\n")
+print (json.dumps(hashtags[0:10], indent=1),"\n")
